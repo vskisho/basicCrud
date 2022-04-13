6c6f9cbb151358e645c5a6c7d417c5977cd370bf
vskisho <vskisho@live.com>
2022-04-13 12:37:03 +0530

    not

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hot reload.
+          Edit <code>src/App.js</code> not reload.
         </p>
         <a
           className="App-link"
